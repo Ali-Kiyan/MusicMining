dd1ffb47a11178b5bdcd7fd7006e609693b11285
Ali <alikiyand@gmail.com>
2017-11-21 13:00:46 +0000

    normalization

--- MusicMining.R
+++ MusicMining.R
@@ -2,6 +2,8 @@ music <- read.csv("music.csv", header=T)
 library(cluster)
 nrml <- music[,-c(2, 3, 4, 5, 6, 14, 15, 16, 17, 18, 19, 20, 23, 25, 27, 30, 31, 33, 34)]
 nrml[nrml$year == "0", "year"] <- NA
-nrml <- music[complete.cases(music),]
+nrml <- nrml[complete.cases(nrml),]
 nrml
 dim(nrml)
+nrml$popular <- ifelse(nrml$song.hotttnesss > 0.5, 1, 0)
+str(nrml)
